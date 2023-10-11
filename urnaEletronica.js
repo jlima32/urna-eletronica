02dc0bb68ee7c942734109341612b64ee6c0a9b3
Juan Lima <juangustavo@gmail.com>
2023-10-11 08:27:25 -0300

    feat: função urnaEletronica

--- urnaEletronica.js
+++ urnaEletronica.js
@@ -1,3 +1,35 @@
 function urnaEletronica() {
-    alert("nada");
+    let candidato1 = 0;
+    let candidato2 = 0;
+    let candidato3 = 0;
+    let brancos = 0;
+    let nulos = 0;
+
+    let voto = parseInt(prompt("Digite o número do candidato: "))
+    const msgSucesso = "Voto computado com sucesso!"
+
+
+    while (voto != 0) {
+        if(voto == 1){
+            candidato1++;
+            console.log(msgSucesso);
+        }else if(voto == 2){
+            candidato2++;
+            console.log(msgSucesso);
+        }else if(voto == 3){
+            candidato3++;
+            console.log(msgSucesso);
+        }else if(voto == 5){
+            brancos++;
+            console.log(msgSucesso);
+        }else if(voto == 8){
+            nulos++;
+            console.log(msgSucesso);
+        }else{
+            console.log("Opção inválida")
+        }
+        voto = parseInt(prompt("Digite o número do candidato: "))       
+    }
+
+    console.log("Votação encerrada!!!")
 }
