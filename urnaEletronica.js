59507f7404f7bfad0839a214d413b59c1361f828
Juan Lima <juangustavo@gmail.com>
2023-10-17 11:14:03 -0300

    feat: desafio#2 confirmação para encerrar

--- urnaEletronica.js
+++ urnaEletronica.js
@@ -9,7 +9,6 @@ function nomeCandidatos() {
     document.getElementById('candidato3').innerHTML = `${nomeCandidato3}`;
     document.getElementById('branco').innerHTML = `Brancos`;
     document.getElementById('nulo').innerHTML = `Nulos`;
-    document.getElementById('encerrar').innerHTML = `Encerrar Votação`;
     
 }
 
@@ -23,75 +22,75 @@ function urnaEletronica() {
     let candidato3 = 0;
     let brancos = 0;
     let nulos = 0;
+    let encerrar = "n";
+    
+    while (encerrar == 'n') {
   
     let voto = parseInt(prompt("Digite o número do candidato: "))
     const msgSucesso = "Voto computado com sucesso!"
 
+            if(voto == 1){
+                candidato1++;
+                console.log(msgSucesso);
+            }else if(voto == 2){
+                candidato2++;
+                console.log(msgSucesso);
+            }else if(voto == 3){
+                candidato3++;
+                console.log(msgSucesso);
+            }else if(voto == 5){
+                brancos++;
+                console.log(msgSucesso);
+            }else if(voto == 8){
+                nulos++;
+                console.log(msgSucesso);
+            }else if(voto == 0){
+                encerrar = prompt("Digite S para encerrar e N para continuar a votação");
+            }
+    }
 
-    while (voto != 0) {
-        if(voto == 1){
-            candidato1++;
-            console.log(msgSucesso);
-        }else if(voto == 2){
-            candidato2++;
-            console.log(msgSucesso);
-        }else if(voto == 3){
-            candidato3++;
-            console.log(msgSucesso);
-        }else if(voto == 5){
-            brancos++;
-            console.log(msgSucesso);
-        }else if(voto == 8){
-            nulos++;
-            console.log(msgSucesso);
+
+        const totalVotos = candidato1 + candidato2 + candidato3 + brancos + nulos;
+        const porcentagemCandidato1 = candidato1 / totalVotos * 100;
+        const porcentagemCandidato2 = candidato2 / totalVotos * 100;
+        const porcentagemCandidato3 = candidato3 / totalVotos * 100;
+        const porcentagemBrancos = brancos / totalVotos * 100;
+        const porcentagemNulos = nulos / totalVotos * 100;
+        
+        console.log("============================");
+        console.log("Votação encerrada!");
+        console.log("============================");
+
+
+        if(candidato1 > candidato2 && candidato1 > candidato3){
+            const candidatoVencedor = candidato1;
+            const totalVencedorBrancos = candidatoVencedor + brancos;
+            const porcentagemVencedorBrancos = (totalVencedorBrancos / totalVotos) * 100;
+            console.log(`O vencedor foi o candidato ${nomeCandidato1} com um total de ${totalVencedorBrancos} votos e uma porcentagem de ${porcentagemVencedorBrancos}% (somado os votos em branco)`);
+        }else if(candidato2 > candidato1 && candidato2 > candidato3){
+            const candidatoVencedor = candidato2;
+            const totalVencedorBrancos = candidatoVencedor + brancos;
+            const porcentagemVencedorBrancos = (totalVencedorBrancos / totalVotos) * 100;
+            console.log(`O vencedor foi o candidato ${nomeCandidato2} com um total de ${totalVencedorBrancos} votos e uma porcentagem de ${porcentagemVencedorBrancos}% (somado os votos em branco)`);
+        }else if(candidato3 > candidato1 && candidato3 > candidato2){
+            const candidatoVencedor = candidato3;
+            const totalVencedorBrancos = candidatoVencedor + brancos;
+            const porcentagemVencedorBrancos = (totalVencedorBrancos / totalVotos) * 100;
+            console.log(`O vencedor foi o candidato ${nomeCandidato3} com um total de ${totalVencedorBrancos} votos e uma porcentagem de ${porcentagemVencedorBrancos}% (somado os votos em branco)`);
         }else{
-            console.log("Opção inválida")
+            console.log(`A votação terminou empatada`);
         }
-        voto = parseInt(prompt("Digite o número do candidato: "))       
-    }
 
-    const totalVotos = candidato1 + candidato2 + candidato3 + brancos + nulos;
-    const porcentagemCandidato1 = candidato1 / totalVotos * 100;
-    const porcentagemCandidato2 = candidato2 / totalVotos * 100;
-    const porcentagemCandidato3 = candidato3 / totalVotos * 100;
-    const porcentagemBrancos = brancos / totalVotos * 100;
-    const porcentagemNulos = nulos / totalVotos * 100;
 
+        console.log("============================");   
+        console.log("Resultado final da votação");
+        console.log(`Total de votos: ${totalVotos}`);
+        console.log("===========================");
+        console.log(`Votos ${nomeCandidato1}: ${candidato1} || ${porcentagemCandidato1}%`)
+        console.log(`Votos ${nomeCandidato2}: ${candidato2} || ${porcentagemCandidato2}%`)
+        console.log(`Votos ${nomeCandidato3}: ${candidato3} || ${porcentagemCandidato3}%`)
+        console.log(`Votos Brancos: ${brancos} || ${porcentagemBrancos}%`)
+        console.log(`Votos Nulos: ${nulos} || ${porcentagemNulos}%`)
+        console.log("===========================");
     
-    console.log("============================");
-    console.log("Votação encerrada!");
-    console.log("============================");
-
-
-    if(candidato1 > candidato2 && candidato1 > candidato3){
-        const candidatoVencedor = candidato1;
-        const totalVencedorBrancos = candidatoVencedor + brancos;
-        const porcentagemVencedorBrancos = (totalVencedorBrancos / totalVotos) * 100;
-        console.log(`O vencedor foi o candidato ${nomeCandidato1} com um total de ${totalVencedorBrancos} votos e uma porcentagem de ${porcentagemVencedorBrancos}% (somado os votos em branco)`);
-    }else if(candidato2 > candidato1 && candidato2 > candidato3){
-        const candidatoVencedor = candidato2;
-        const totalVencedorBrancos = candidatoVencedor + brancos;
-        const porcentagemVencedorBrancos = (totalVencedorBrancos / totalVotos) * 100;
-        console.log(`O vencedor foi o candidato ${nomeCandidato2} com um total de ${totalVencedorBrancos} votos e uma porcentagem de ${porcentagemVencedorBrancos}% (somado os votos em branco)`);
-    }else if(candidato3 > candidato1 && candidato3 > candidato2){
-        const candidatoVencedor = candidato3;
-        const totalVencedorBrancos = candidatoVencedor + brancos;
-        const porcentagemVencedorBrancos = (totalVencedorBrancos / totalVotos) * 100;
-        console.log(`O vencedor foi o candidato ${nomeCandidato3} com um total de ${totalVencedorBrancos} votos e uma porcentagem de ${porcentagemVencedorBrancos}% (somado os votos em branco)`);
-    }else{
-        console.log(`A votação terminou empatada`);
-    }
-
-
-    console.log("============================");   
-    console.log("Resultado final da votação");
-    console.log(`Total de votos: ${totalVotos}`);
-    console.log("===========================");
-    console.log(`Votos ${nomeCandidato1}: ${candidato1} || ${porcentagemCandidato1}%`)
-    console.log(`Votos ${nomeCandidato2}: ${candidato2} || ${porcentagemCandidato2}%`)
-    console.log(`Votos ${nomeCandidato3}: ${candidato3} || ${porcentagemCandidato3}%`)
-    console.log(`Votos Brancos: ${brancos} || ${porcentagemBrancos}%`)
-    console.log(`Votos Nulos: ${nulos} || ${porcentagemNulos}%`)
-    console.log("===========================");
-
 }
